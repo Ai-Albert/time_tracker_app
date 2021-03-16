d9efbfaba6545efabae27cb37aa1a37ec1d4a4a6
Albert Ai <gsbalbert2002@gmail.com>
2021-03-15 22:16:57 -0700

    Update sign_in_page.dart

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -6,6 +6,21 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: Colors.orange,
+              child: SizedBox(
+                height: 100,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
