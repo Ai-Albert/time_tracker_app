71d702a60a1f6a4a15a663403aa8731d2b985839
Albert Ai <gsbalbert2002@gmail.com>
2021-03-16 19:53:57 -0700

    Added sign in buttons

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/app/sign_in/sign_in_button.dart';
+import 'package:time_tracker/common_widgets/custom_elevated_button.dart';
 
 class SignInPage extends StatelessWidget {
   @override
@@ -8,19 +10,71 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
-      body: Container(
-        color: Colors.yellow,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              color: Colors.orange,
-              child: SizedBox(
-                height: 100,
-              ),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Sign in',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
             ),
-          ],
-        ),
+          ),
+          SizedBox(height: 48.0),
+
+          // Buttons start below
+          SignInButton(
+            text: 'Sign in with Google',
+            color: Colors.white,
+            textColor: Colors.black87,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+
+          SignInButton(
+            text: 'Sign in with Facebook',
+            color: Color(0xFF334D92),
+            textColor: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+
+          SignInButton(
+            text: 'Sign in with email',
+            color: Colors.teal[700],
+            textColor: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+
+          Text(
+            'or',
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.black87,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 8.0),
+
+          SignInButton(
+            text: 'Go anonymous',
+            color: Colors.lime[300],
+            textColor: Colors.black,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+        ],
       ),
     );
   }
---
+++ lib/common_widgets/custom_elevated_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+
+  final Widget child;
+  final Color color;
+  final double borderRadius;
+  final double height;
+  final VoidCallback onPressed;
+
+  CustomElevatedButton({
+    this.child,
+    this.color,
+    this.borderRadius: 2.0,
+    this.height: 50.0,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: this.height,
+      child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(this.color),
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(this.borderRadius)),
+            )),
+          ),
+          child: this.child,
+          onPressed: this.onPressed,
+        ),
+    );
+  }
+}
