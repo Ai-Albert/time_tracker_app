c2c1da009356691214bada807fe82c22d53ca41f
Albert Ai <gsbalbert2002@gmail.com>
2021-05-24 22:32:51 -0700

    Added basic job adding functionality

---
+++ lib/app/home/jobs/jobs_page.dart
@@ -0,0 +1,89 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker/common_widgets/show_alert_dialog.dart';
+import 'package:time_tracker/common_widgets/show_exception_alert_dialog.dart';
+import 'package:time_tracker/services/auth.dart';
+import 'package:time_tracker/services/database.dart';
+import 'models/job.dart';
+
+class JobsPage extends StatelessWidget {
+
+  Future _signOut(BuildContext context) async {
+    try {
+      await Provider.of<AuthBase>(context, listen: false).signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<bool> _confirmSignOut(BuildContext context) async {
+    final request = await showAlertDialog(
+      context,
+      title: 'Logout',
+      content: 'Are you sure you want to log out?',
+      cancelActionText: 'Cancel',
+      defaultActionText: 'Logout',
+    );
+    if (request == true) {
+      _signOut(context);
+    }
+  }
+
+  Future<void> _createJob(BuildContext context) async {
+    try {
+      final database = Provider.of<Database>(context, listen: false);
+      await database.createJob(Job(name: 'Blogging', ratePerHour: 10));
+    } on FirebaseException catch(e) {
+      showExceptionAlertDialog(
+        context,
+        title: 'Operation failed',
+        exception: e,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Jobs'),
+        actions: <Widget>[
+          TextButton(
+            child: Text(
+              'logout',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () => _confirmSignOut(context),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _createJob(context),
+      ),
+      body: _buildContents(context),
+    );
+  }
+
+  Widget _buildContents(BuildContext context) {
+    final database = Provider.of<Database>(context, listen: false);
+    return StreamBuilder<List<Job>>(
+      stream: database.jobsStream(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final jobs = snapshot.data;
+          final children = jobs.map((job) => Text(job.name)).toList();
+          return ListView(children: children);
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text('Some error occurred'));
+        }
+        return Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+}
