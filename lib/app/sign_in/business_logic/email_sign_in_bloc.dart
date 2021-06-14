887c50973e699e572a87788411a00aaaf3d444fe
Albert Ai <gsbalbert2002@gmail.com>
2021-06-14 13:18:27 -0700

    Updated email sign in bloc to use BehaviorSubject from RxDart which only remembers the last value it was given

--- lib/app/sign_in/business_logic/email_sign_in_bloc.dart
+++ lib/app/sign_in/business_logic/email_sign_in_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:time_tracker/app/sign_in/old/email_sign_in_model.dart';
 import 'package:time_tracker/services/auth.dart';
 
@@ -8,12 +9,13 @@ class EmailSignInBloc {
   EmailSignInBloc({@required this.auth});
 
   final AuthBase auth;
-  final StreamController<EmailSignInModel> _modelController = StreamController<EmailSignInModel>();
-  Stream<EmailSignInModel> get modelStream => _modelController.stream;
-  EmailSignInModel _model = EmailSignInModel();
+
+  final _modelSubject = BehaviorSubject<EmailSignInModel>.seeded(EmailSignInModel());
+  Stream<EmailSignInModel> get modelStream => _modelSubject.stream;
+  EmailSignInModel get _model => _modelSubject.value;
 
   void dispose() {
-    _modelController.close();
+    _modelSubject.close();
   }
 
   void changeForm() {
@@ -40,15 +42,13 @@ class EmailSignInBloc {
     bool submitted,
   }) {
     // update model
-    _model = _model.copyWith(
+    _modelSubject.value = _model.copyWith(
       email: email,
       password: password,
       formType: formType,
       isLoading: isLoading,
       submitted: submitted,
     );
-    //add updated model to _modelController
-    _modelController.add(_model);
   }
 
   Future<void> submit() async {
